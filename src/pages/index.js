0ceec91d669dc859dc8b6696941e72a0d6fcb858
CTCbaxia <ctcbaxia@gmail.com>
2020-02-22 23:38:46 +0800

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div>
+        Hello world!
+        <h1>Ok, let me to do this to try</h1>
+        <h2> can I add one more line</h2>
+        <h2> can I remove another line</h2>
+    </div>
+    
+)
